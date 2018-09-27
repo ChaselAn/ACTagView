e10f0ca7977e88391d349fc68ba2698f4b8745e7
ancheng <cheng.an@duodian.com>
2018-09-27 16:03:33 +0800

    change to framework

---
+++ ACTagViewDemo/ACTagView/ACTagView.h
@@ -0,0 +1,19 @@
+//
+//  ACTagView.h
+//  ACTagView
+//
+//  Created by ancheng on 2018/9/27.
+//  Copyright © 2018年 ac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ACTagView.
+FOUNDATION_EXPORT double ACTagViewVersionNumber;
+
+//! Project version string for ACTagView.
+FOUNDATION_EXPORT const unsigned char ACTagViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACTagView/PublicHeader.h>
+
+
--- ACTagViewDemo/ACTagViewDemo/TagViewController.swift
+++ ACTagViewDemo/ACTagViewDemo/TagViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ACTagView
 
 class TagViewController: UIViewController {
   
